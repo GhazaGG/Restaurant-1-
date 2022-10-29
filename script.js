feb8aebc395a71e622f4682a27e7b7ccceb40f98
GhazaGG <ghazagg7@gmail.com>
2022-10-29 14:37:50 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+let cartItem = document.querySelector('.cart-items-container');
+
+document.querySelector('#cart-btn').onclick = () =>{
+    cartItem.classList.toggle('active');
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
+}
